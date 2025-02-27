26a8888b058a351e0ed65b1d2e5795ce2dee4086
Renata <renata.rodriguesdasilva@outlook.com>
2025-02-26 23:23:59 -0300

    feat: update

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-3xl text-brand">
-        StoreIt - The only storage solution you need.
-      </h1>
+    <div className="center">
+      <h1 className="h1">StoreIt - The only storage solution you need.</h1>
     </div>
   );
 }
