2ebef9d4da49f9f28c18f99b2b8c0263847771a9
Renata <renata.rodriguesdasilva@outlook.com>
2025-03-01 10:15:29 -0300

    feat: Update

---
+++ lib/appwrite/index.ts
@@ -0,0 +1,25 @@
+import { Client, Databases } from "node-appwrite";
+import { appwriteConfig } from "./config";
+
+export const createSessionClient = async () => {
+  const client = new Client()
+    .setEndpoint(appwriteConfig.endpointUrl)
+    .setProject(appwriteConfig.projectId);
+
+  const session = (await cookies()).get("appwrite-");
+
+  if (!session || !session.value) throw new Error("No session");
+
+  client.setSession(session.value);
+
+  return {
+    get account() {
+      return new Account(client);
+    },
+    get database() {
+      return new Databases(client);
+    },
+  };
+};
+
+export const createAdminClient = async () => {};
