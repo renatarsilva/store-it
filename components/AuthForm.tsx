7127058abcfb602fde44f7cafb66d1d03e71f5ed
Renata <renata.rodriguesdasilva@outlook.com>
2025-03-01 18:27:56 -0300

    feat: Update AuthForm

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -15,9 +15,10 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { useState } from "react";
+import { useState, useSyncExternalStore } from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { createAccount } from "@/lib/actions/user.actions";
 
 type FormType = "sign-in" | "sign-up";
 
@@ -34,6 +35,7 @@ const authFormSchema = (formType: FormType) => {
 const AuthForm = ({ type }: { type: FormType }) => {
   const [isLoading, setIsLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
+  const [accountId, setAccountId] = useState(null);
 
   const formSchema = authFormSchema(type);
   const form = useForm<z.infer<typeof formSchema>>({
@@ -45,7 +47,21 @@ const AuthForm = ({ type }: { type: FormType }) => {
   });
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
-    console.log(values);
+    setIsLoading(true);
+    setErrorMessage("");
+
+    try {
+      const user = await createAccount({
+        fullName: values.fullName || " ",
+        email: values.email,
+      });
+
+      setAccountId(user.accountId);
+    } catch {
+      setErrorMessage("Failed to create account. Please try again");
+    } finally {
+      setIsLoading(false);
+    }
   };
   return (
     <>
--- lib/appwrite/config.ts
+++ lib/appwrite/config.ts
@@ -7,5 +7,5 @@ export const appwriteConfig = {
   filesCollectionId: (process.env.NEXT_PUBLIC_APPWRITE_FILES_COLLECTION =
     "67c1087c00336fa14d89"!),
   bucketId: (process.env.NEXT_PUBLIC_APPWRITE_BUCKET = "67c10bcf000431b62a80"!),
-  createSecretKey: process.env.NEXT_APPWRITE_KEY!,
+  secretKey: process.env.NEXT_APPWRITE_KEY!,
 };
