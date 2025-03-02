34bd1d495ca78085619356572fbc2e4b6cf544be
Renata <renata.rodriguesdasilva@outlook.com>
2025-03-02 02:07:43 -0300

    feat: update

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -28,9 +28,11 @@ const Sidebar = () => {
       </Link>
       <nav className="sidebar-nav">
         <ul className="flex flex-1 flex-col gap-6">
-          {navItems.map((item) => {
-            const active = pathname === item.url;
-          })}
+          {navItems.map(({ url, name, icon }) => (
+            <Link key={url} href={url}>
+              <li></li>
+            </Link>
+          ))}
         </ul>
       </nav>
     </aside>
