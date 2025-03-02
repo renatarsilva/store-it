2f8a934dadd907f9c20675d84471d4de89b86ed5
Renata <renata.rodriguesdasilva@outlook.com>
2025-03-02 01:54:58 -0300

    feat: Update

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,7 @@
 import Header from "@/components/Header";
 import MobileNavigation from "@/components/MobileNavigation";
-import { Sidebar } from "lucide-react";
+import Sidebar from "@/components/Sidebar";
+
 import React from "react";
 
 const layout = ({ children }: { children: React.ReactNode }) => {
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,8 +1,13 @@
+"use client";
+
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
+import { navItems } from "@/constants";
+import { usePathname } from "next/navigation";
 
 const Sidebar = () => {
+  const pathname = usePathname();
   return (
     <aside className="sidebar">
       <Link href="/">
@@ -21,9 +26,12 @@ const Sidebar = () => {
           className="lg:hidden"
         />
       </Link>
-
       <nav className="sidebar-nav">
-        <ul className="flex flex-1 flex-col gap-6"></ul>
+        <ul className="flex flex-1 flex-col gap-6">
+          {navItems.map((item) => {
+            const active = pathname === item.url;
+          })}
+        </ul>
       </nav>
     </aside>
   );
