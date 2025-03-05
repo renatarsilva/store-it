706be152ee86e359ca25f7be6d744680610fc270
Renata <renata.rodriguesdasilva@outlook.com>
2025-03-05 16:57:47 -0300

    feat: updates

---
+++ app/(root)/[type]/page.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Sort from "@/components/Sort";
+import { getFiles } from "@/lib/actions/file.actions";
+
+const Page = async ({ params }: SearchParamProps) => {
+  const type = ((await params)?.type as string) || "";
+
+  const files = await getFiles();
+
+  return (
+    <div className="page-container">
+      <section className="w-full">
+        <h1 className="h1 capitalize">{type}</h1>
+        <div className="total-size-section">
+          <p className="body-1">
+            Total: <span className="h5">0 MB</span>
+          </p>
+
+          <div className="sort-container">
+            <p className="body-1 hidden text-light-200 sm:block">Sort by:</p>
+
+            <Sort />
+          </div>
+        </div>
+      </section>
+      {/* Render the files */}
+    </div>
+  );
+};
+
+export default Page;
