8b9e6dad88e15ae2f8aacb860abea1b1cd388ba1
Renata <renata.rodriguesdasilva@outlook.com>
2025-03-06 13:11:38 -0300

    feat: update

--- components/ActionDropdown.tsx
+++ components/ActionDropdown.tsx
@@ -23,6 +23,7 @@ import {
 import { Button } from "./ui/button";
 import { usePathname } from "next/navigation";
 import { renameFile } from "@/lib/actions/file.actions";
+import { FileDetails } from "./ActionsModalContent";
 
 const ActionDropdown = ({ file }: { file: Models.Document }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -79,6 +80,7 @@ const ActionDropdown = ({ file }: { file: Models.Document }) => {
               onChange={(e) => setName(e.target.value)}
             />
           )}
+          {value && "details" && <FileDetails file={file} />}
         </DialogHeader>
         {["rename", "delete", "share"].includes(value) && (
           <DialogFooter className="flex flex-col gap-3 md:flex-row">
